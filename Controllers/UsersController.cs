6332ec894287616ccb77f05ca99318d65394915d
ericbr0wne <ericbrowne@hotmail.com>
2024-07-16 21:55:29 +0200

    UsersControllers - PUT and DELETE actions

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -24,5 +24,33 @@ namespace dotSkog.Controllers
             return await _context.Users.ToListAsync();
         }
 
+        //PUT: api/users/{id}
+        [HttpPut("{id}")] // update a user
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent(); // GREAT SUCCESS, no content to return
+        }
+
+        //DELETE: api/users/{id}
+        [HttpDelete("{id}")] // Delete a user
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null )
+            {
+                return NotFound();               
+            }
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    
     }
 }
